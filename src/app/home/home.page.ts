3ca89293f39a43ade5d22ca71d3578e045e38a9a
mirko1987 <uriel873@virgilio.it>
2019-06-24 12:37:29 +0200

    prova help center

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
-
+import { Component } from "@angular/core";
+import { InAppBrowser } from "@ionic-native/in-app-browser/ngx";
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"]
 })
 export class HomePage {
+  constructor(private iab: InAppBrowser) {}
 
-  constructor() {}
-
+  helpCenter() {
+    console.log("prova");
+    this.iab.create(`https://rideapp.zendesk.com/hc/it`, `_blank`);
+  }
 }
